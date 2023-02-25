2e26e6697c4d166ecbc5c6e2d31392415239469c
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-02-25 14:25:46 +0530

    Make App a class component instead of a function component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React from 'react';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {LoginCard} from './CardComponents';
+import { LoginCard } from './CardComponents';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 id="mainHeading">MoodleArchiver</h1>
-        <LoginCard></LoginCard>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 id="mainHeading">MoodleArchiver</h1>
+          <LoginCard></LoginCard>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
