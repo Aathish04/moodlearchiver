f23fbb12d2e23bd2e3b074c411570c0131633205
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-02-25 17:02:24 +0530

    Obtain UserID using token.

--- src/MoodleAPI.js
+++ src/MoodleAPI.js
@@ -5,12 +5,13 @@ export class MoodleClient {
     }
     constructor(username, backend) {
         this.username = username;
+        this.userid = null;
         this.token = null;
         this.backend = backend.replace("/+$/g", '');
     }
 
     async getToken(password) {
-        let bodyContent = new FormData();
+        let bodyContent = new MoodleFormData();
         bodyContent.append("username", this.username);
         bodyContent.append("password", password);
         bodyContent.append("service", "moodle_mobile_app");
@@ -26,5 +27,39 @@ export class MoodleClient {
         else if ("error" in data) {
             throw new Error(data["errorcode"])
         }
+    }
+
+    async getUserID() {
+        if (this.token === null) {
+            throw new Error("token not recieved yet");
+        }
+        if (this.userid === null) {
+            let bodyContent = new MoodleFormData();
+            bodyContent.append("wstoken", this.token);
+            bodyContent.append("wsfunction", "core_webservice_get_site_info");
+
+            let response = await fetch("https://lms.ssn.edu.in/webservice/rest/server.php", {
+                method: "POST",
+                body: bodyContent,
+                headers: this.headersList
+            });
+
+            let data = await response.json();
+            if ("errorcode" in data) {
+                throw new Error(data["errorcode"])
+            }
+            this.userid = data["userid"]
+            return this.userid;
+        }
+        else {
+            return this.userid
+        }
+    }
+}
+
+class MoodleFormData extends FormData {
+    constructor(args) {
+        super(args);
+        this.append("moodlewsrestformat", "json")
     }
 }
