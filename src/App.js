49a3397f35a122af8935088c8a46d9704329fdb1
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-02-26 11:11:41 +0530

    Add self-credit and contribution links.

--- src/App.js
+++ src/App.js
@@ -25,12 +25,16 @@ export class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img id="title" src={TitleSVG} alt="MoodleArchiver" className="m-3"/>
+          <img id="title" src={TitleSVG} alt="MoodleArchiver" className="m-3" />
           <LoginCard setMoodleClient={this.setMoodleClient} setLoading={this.setLoading}></LoginCard>
           {
             this.state.moodleclient !== null && <CourseSelectCard moodleclient={this.state.moodleclient} setLoading={this.setLoading}></CourseSelectCard>
           }
         </header>
+        <div className='footer'>
+          <p className="text-start text-light font-monospace" id="self-credit-msg">By: <a href="https://github.com/Aathish04" target="_blank" rel="noopener noreferrer" className="link-onhover-color-change link">Aathish04</a></p>
+          <p className="text-start text-light font-monospace" id="contribute-msg">Feel free to <a href="https://github.com/Aathish04/moodlearchiver" target="_blank" rel="noopener noreferrer" className="link-onhover-color-change link">Contribute</a>!</p>
+        </div>
         {this.state.loading && <LoadingModal></LoadingModal>}
       </div>
     );
