8bacb450e549387fb65cd190606f678fd6fc16c0
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-02-26 17:43:16 +0530

    Use cookies for username custombackend and backend

--- src/CardComponents.js
+++ src/CardComponents.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { instanceOf } from 'prop-types';
+import { Cookies } from 'react-cookie';
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
@@ -10,18 +12,21 @@ import { InvalidCredentialsToast } from "./Toasts"
 import { MoodleClient } from './MoodleAPI';
 
 export class LoginCard extends React.Component { // Custom Card for Login
-
+    static propTypes = {
+        cookies: instanceOf(Cookies).isRequired
+    };
 
     constructor(props) { // Props are inherited properties one could pass.
         super(props)
+        const { cookies } = props;
         // Set the default state here
         this.state = {
             invalidcreds: false,
             wentthruvalidationbefore: false,
-            username: null,
+            username: cookies.get("username") || null,
             password: null,
-            backend: "https://lms.ssn.edu.in/",
-            custombackend: false,
+            backend: cookies.get("backend") || "https://lms.ssn.edu.in/",
+            custombackend: (cookies.get("custombackend") && JSON.parse(cookies.get("custombackend"))) || false,
             borderstyle: "light"
         }
 
@@ -43,6 +48,9 @@ export class LoginCard extends React.Component { // Custom Card for Login
         this.setState({ password: event.target.value });
     }
     handleBackendSelectorChange(event) {
+        // Backend has changed, so we have to reset the
+        // moodle client.
+        this.props.setMoodleClient(null);
         if (event.target.value === "custom") {
             this.setState({ custombackend: true });
         }
@@ -54,6 +62,7 @@ export class LoginCard extends React.Component { // Custom Card for Login
     }
     async handleSubmit(event) {
         event.preventDefault();
+        const { cookies } = this.props;
         const form = event.currentTarget;
         if (form.checkValidity() === false) {
             this.setState({ wentthruvalidationbefore: false });
@@ -70,6 +79,11 @@ export class LoginCard extends React.Component { // Custom Card for Login
                 this.props.setLoading(false)
                 this.setState({ borderstyle: "success", invalidcreds: false });
                 this.setMoodleClient(moodleclient);
+
+                let oneyearlater = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+                cookies.set('username', this.state.username, { expires: oneyearlater });
+                cookies.set('custombackend', this.state.custombackend, { expires: oneyearlater });
+                cookies.set('backend', this.state.backend, { expires: oneyearlater });
             }
             catch (e) {
                 if (e.message === "invalidlogin") {
@@ -85,6 +99,7 @@ export class LoginCard extends React.Component { // Custom Card for Login
     }
 
     render() {
+        const { cookies } = this.props;
         return <Card className='m-2' border={this.state.borderstyle}>
             <Card.Header>Login to Moodle</Card.Header>
             <Form onSubmit={this.handleSubmit} noValidate validated={this.state.wentthruvalidationbefore}>
@@ -109,7 +124,11 @@ export class LoginCard extends React.Component { // Custom Card for Login
 
                 <Form.Group className="m-3" controlId="moodlebackendselectform">
                     <Form.Label>Moodle Backend:</Form.Label>
-                    <Form.Select aria-label="Moodle Backend Selector" onChange={this.handleBackendSelectorChange} defaultValue={this.state.backend} required>
+                    <Form.Select
+                        aria-label="Moodle Backend Selector"
+                        onChange={this.handleBackendSelectorChange}
+                        defaultValue={this.state.custombackend ? "custom" : this.state.backend}
+                        required>
                         <option value="https://lms.ssn.edu.in/">SSN College LMS</option>
                         <option value="https://lms-old.ssn.edu.in/">SSN College LMS-Old</option>
                         <option value="custom">Custom Backend (Experimental!)</option>
@@ -118,7 +137,12 @@ export class LoginCard extends React.Component { // Custom Card for Login
                 {
                     this.state.custombackend &&
                     <Form.Group className="m-3" controlId="moodlecustombackendform">
-                        <Form.Control type="url" placeholder="https://school.moodledemo.net/" onChange={this.handleCustomBackendInput} required={this.state.custombackend} />
+                        <Form.Control
+                            type="url"
+                            placeholder="https://school.moodledemo.net/"
+                            defaultValue={cookies.get("custombackend") && JSON.parse(cookies.get("custombackend")) ? this.state.backend : undefined}
+                            onChange={this.handleCustomBackendInput}
+                            required={this.state.custombackend} />
                         <Form.Control.Feedback type="invalid">
                             Please enter a proper URL.
                         </Form.Control.Feedback>
